4150568682dbe68ec2ee6e5be8b837d1462122b0
andre-kumagae <andre.kumagae@hotmail.com>
2026-04-18 13:28:30 -0300

    deletado projeto spring

--- cursos/plataformas/javanauta/combo-fullstack-java/usuario/src/main/java/com/javanauta/usuario/infrastructure/repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javanauta.usuario.infrastructure.repository;
-
-import com.javanauta.aprendendo_spring.infrastructure.entity.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}

e5879ecbad69cbda668935d385b295ef6028ac0f
andre-kumagae <andre.kumagae@hotmail.com>
2026-05-09 18:54:14 -0300

    Módulo Orientação a Objetos em Java - Encapsulamento

--- cursos/plataformas/javanauta/combo-fullstack-java/primeiro-projeto/modulos_jdk17/src/logica_de_programacao/m05_orientacao_a_objetos_em_java/aula_04_encapsulamento/Agencia.java
+++ cursos/plataformas/javanauta/combo-fullstack-java/primeiro-projeto/modulos_jdk17/src/logica_de_programacao/m05_orientacao_a_objetos_em_java/aula_04_encapsulamento/Agencia.java
@@ -1,4 +1,11 @@
 package logica_de_programacao.m05_orientacao_a_objetos_em_java.aula_04_encapsulamento;
 
 public class Agencia {
+    public static void main(String[] args) {
+        ContaBancaria minhaConta = new ContaBancaria("Carlos", 500);
+        minhaConta.setSaldo(minhaConta.getSaldo() + 500);
+        System.out.println(minhaConta.getTitular());
+        System.out.println(minhaConta.getSaldo());
+        System.out.println(minhaConta);
+    }
 }
--- cursos/plataformas/javanauta/combo-fullstack-java/primeiro-projeto/modulos_jdk17/src/logica_de_programacao/m05_orientacao_a_objetos_em_java/aula_04_encapsulamento/ContaBancaria.java
+++ cursos/plataformas/javanauta/combo-fullstack-java/primeiro-projeto/modulos_jdk17/src/logica_de_programacao/m05_orientacao_a_objetos_em_java/aula_04_encapsulamento/ContaBancaria.java
@@ -1,4 +1,43 @@
 package logica_de_programacao.m05_orientacao_a_objetos_em_java.aula_04_encapsulamento;
 
 public class ContaBancaria {
+    private String titular;
+    private double saldo;
+
+    public ContaBancaria(String titular, double saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public ContaBancaria() {
+    }
+
+    public ContaBancaria(String titular) {
+        this.titular = titular;
+        this.saldo = 0;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "ContaBancaria{" +
+                "titular='" + titular + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
 }

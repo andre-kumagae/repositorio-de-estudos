a55484b9576683f671e9dda52760f5f308891aa2
andre-kumagae <andre.kumagae@hotmail.com>
2026-05-14 00:06:02 -0300

    solved exercise from chapter 1.9 - Programming Style and Documentation

---
+++ livros/introduction-to-java-programming-and-data-structures/src/ch01/lesson_exercises/ex01_09/Test.java
@@ -0,0 +1,9 @@
+package ch01.lesson_exercises.ex01_09;
+
+public class Test {
+    // Main method
+    public static void main(String[] args) {
+        /** Display output */
+        System.out.println("Welcome to Java");
+    }
+}
